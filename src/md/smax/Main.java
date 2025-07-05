a25a37cebf8fb95ee9b519cf7814c131fb201e65
Daniel <danielmihai.it@mail.ru>
2025-07-05 19:27:41 +0300

    Student Services

--- src/md/smax/Main.java
+++ src/md/smax/Main.java
@@ -1,5 +1,6 @@
 package md.smax;
 import md.smax.model.Student;
+import md.smax.service.StudentService;
 
 public class Main {
     public static void main(String[] args) {
--- src/md/smax/service/StudentService.java
+++ src/md/smax/service/StudentService.java
@@ -1,4 +1,42 @@
 package md.smax.service;
+import md.smax.model.Student;
 
 public class StudentService {
+//    Поля
+    int studentsCount = 0;
+    int gradeCount = 0;
+    int maxGrade = Integer.MIN_VALUE;
+    int minGrade = Integer.MAX_VALUE;
+    Student maxGradeStudent = null;
+    Student minGradeStudent = null;
+
+    //    Методы
+    public void addGrade(Student student, int grade) {
+        studentsCount++;
+        gradeCount += grade;
+
+        if (grade > maxGrade) {
+            maxGrade = grade;
+            maxGradeStudent = student;
+        }
+
+
+        if (grade < minGrade) {
+            minGrade = grade;
+            minGradeStudent = student;
+        }
+    }
+
+    public float getAverageGrade() {
+        float averageGrade = gradeCount / studentsCount;
+        return averageGrade;
+    }
+
+    public Student getBestStudent() {
+        return maxGradeStudent;
+    }
+
+    public Student getWorstStudent() {
+        return minGradeStudent;
+    }
 }
