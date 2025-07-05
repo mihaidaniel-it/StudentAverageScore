9721b39a0f43b287b289c3d22615e31a74639c9d
Daniel <danielmihai.it@mail.ru>
2025-07-05 18:47:40 +0300

    Class Student functions

--- src/md/smax/Main.java
+++ src/md/smax/Main.java
@@ -1,4 +1,8 @@
 package md.smax;
+import md.smax.model.Student;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
--- src/md/smax/model/Student.java
+++ src/md/smax/model/Student.java
@@ -1,7 +1,31 @@
 package md.smax.model;
 
 public class Student {
-    public static void main(String[] args) {
+    //    Поля
+    public String studentName;
+    public int studentClass;
 
+//    Конструктор
+public Student(String studentName, int studentClass) {
+        this.studentName = studentName;
+        this.studentClass = studentClass;
+    }
+
+//    Гетеры
+    public String getStudentName() {
+        return this.studentName;
+    }
+
+    public int getStudentClass() {
+        return this.studentClass;
+    }
+
+//    Cетеры
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public void setStudentClass(int studentClass) {
+        this.studentClass = studentClass;
     }
 }
